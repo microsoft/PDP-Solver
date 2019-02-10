8d3bfef1ebb3cadb7e13c521f57cd6f99f35b24d
Saeed Amizadeh <saeed@MacBook-Pro.local>
2019-02-09 17:42:55 -0800

    code clean-up, comments added 2

--- src/PDP.py
+++ src/PDP.py
@@ -1,3 +1,8 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+
+# PDP.py : The main entry point to the PDP trainer/tester/predicter.
+
 import argparse
 from scripts_pytorch import PDP_solver_trainer
 
