ca7ecfaf312ad5498f7515ef0f46d572fdbf0ea5
Sergiy Matusevych <sergiy.matusevych@gmail.com>
2019-02-28 18:30:43 -0800

    bugfix: use the right class in super() in PerceptronTanh constructor

--- src/model_pytorch/PDP/util.py
+++ src/model_pytorch/PDP/util.py
@@ -252,7 +252,7 @@ class PerceptronTanh(nn.Module):
     "Implements a 1-layer perceptron with Tanh activaton."
 
     def __init__(self, input_dimension, hidden_dimension, output_dimension):
-        super(Perceptron, self).__init__()
+        super(PerceptronTanh, self).__init__()
         self._layer1 = nn.Linear(input_dimension, hidden_dimension)
         self._layer2 = nn.Linear(hidden_dimension, output_dimension, bias=False)
 
